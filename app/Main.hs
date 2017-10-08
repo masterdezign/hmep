4c3c6bb3cf0a0ebc5845cb11fab67cca46427e46
Bogdan <masterdezign@gmail.com>
2017-10-08 18:57:31 +0200

    Remove hmatrix from package dependencies to simplify the package build

--- app/Main.hs
+++ app/Main.hs
@@ -10,33 +10,25 @@ module Main where
 import qualified Data.Vector as V
 import           Data.List ( foldl' )
 import           Control.Monad ( foldM )
-import           Numeric.LinearAlgebra
-                 ( randomVector
-                 , RandDist( Uniform )
-                 , toList
+import           Math.Probable.Random  -- From `probable` package
+                 ( vectorOf
+                 , double
                  )
 
 import           AI.MEP
 
-ops = V.fromList [('*', (*)), ('+', (+)), ('/', (/)), ('-', (-)),
-  ('s', \x _ -> sin x)]
-
 config = defaultConfig {
-  c'ops = ops
+  -- Functions available to genetically produced programs
+  c'ops = V.fromList [
+       ('*', (*)),
+       ('+', (+)),
+       ('/', (/)),
+       ('-', (-)),
+       ('s', \x _ -> sin x)
+     ]
+  -- Chromosome length
   , c'length = 50
   }
-
--- Feel free to change the random number generation seed
-seed :: Int
-seed = 3
-
-randDomain :: Int -> [Double]
-randDomain = map (subtract pi. (2*pi *)). toList. randomVector seed Uniform
-
-dataset1 :: V.Vector (Double, Double)
-dataset1 = V.map (\x -> (x, function x)) $ V.fromList $ randDomain nSamples
-  where nSamples = 50
-        function x = (cos x)^2
 
 -- | Absolute value distance between two scalar values
 dist :: Double -> Double -> Double
@@ -45,19 +37,6 @@ dist x y = if isNaN x || isNaN y
   then 10000
   else abs $ x - y
 
-loss :: LossFunction Double
-loss evalf = (V.singleton i', loss')
-  where
-    (xs, ys) = unzip $ V.toList dataset1
-    -- Distances resulting from multiple expression evaluation
-    dss = zipWith (\x y -> V.map (dist y). evalf. V.singleton $ x) xs ys
-    -- Cumulative distances for each index
-    dcumul = sum' dss
-    -- Select index minimizing cumulative distances
-    i' = V.minIndex dcumul
-    -- The loss value with respect to the index of the best expression
-    loss' = dcumul V.! i'
-
 -- Could be optimized
 sum' :: Num a => [V.Vector a] -> V.Vector a
 sum' xss = foldl' (V.zipWith (+)) base xss
@@ -65,24 +44,50 @@ sum' xss = foldl' (V.zipWith (+)) base xss
     len = V.length $ head xss
     base = V.replicate len 0
 
-nextGeneration
-  :: [Phenotype Double] -> RandT IO [Phenotype Double]
-nextGeneration = evolve config loss (mutation3 config) crossover binaryTournament
-
-runIO
-  :: [Phenotype Double] -> Int -> IO [Phenotype Double]
-runIO pop i = do
-  newPop <- runRandIO $ foldM (\xg _ -> nextGeneration xg) pop [1..generations]
-  putStrLn $ "Population " ++ show (i * generations) ++ ": average loss " ++ show (avgLoss newPop)
-  return newPop
-    where generations = 5
-
 main :: IO ()
 main = do
+  -- A vector of 50 random numbers between 0 and 1 (including 1)
+  xs <- runRandIO (vectorOf 50 double)
+
+  -- Scale the values to the interval of (-pi, pi]
+  let xs' = V.map ((2*pi *). subtract 0.5) xs
+      -- Target function f to approximate
+      function x = (cos x)^2
+      -- Pairs (x, f(x))
+      dataset = V.map (\x -> (x, function x)) xs'
+
+  -- Randomly create a population of chromosomes
   pop <- runRandIO $ initialize config
+
+  let -- The loss function which depends on the dataset
+      loss evalf = (V.singleton i', loss')
+        where
+          (xs, ys) = unzip $ V.toList dataset
+          -- Distances resulting from multiple expression evaluation
+          dss = zipWith (\x y -> V.map (dist y). evalf. V.singleton $ x) xs ys
+          -- Cumulative distances for each index
+          dcumul = sum' dss
+          -- Select index minimizing cumulative distances
+          i' = V.minIndex dcumul
+          -- The loss value with respect to the index of the best expression
+          loss' = dcumul V.! i'
+
+  -- Evaluate the initial population
   let popEvaluated = evaluateGeneration loss pop
+
   putStrLn $ "Average loss in the initial population " ++ show (avgLoss popEvaluated)
 
+  -- Declare how to produce the new generation
+  let nextGeneration = evolve config loss (mutation3 config) crossover binaryTournament
+
+  -- Specify the I/O loop, which logs every 5 generation
+      runIO pop i = do
+        newPop <- runRandIO $ foldM (\xg _ -> nextGeneration xg) pop [1..generations]
+        putStrLn $ "Population " ++ show (i * generations) ++ ": average loss " ++ show (avgLoss newPop)
+        return newPop
+          where generations = 5
+
+  -- The final population
   final <- foldM runIO popEvaluated [1..20]
   let best = last final
   print best
