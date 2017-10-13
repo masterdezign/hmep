270e6890db2e9ec64e0dde5843ebf8c0aa0a1bdb
Bogdan <masterdezign@gmail.com>
2017-10-13 15:34:07 +0200

    Add a comparison with the Taylor expansion

--- README.md
+++ README.md
@@ -126,7 +126,7 @@ addition and multiplication operators, i.e. with polynomials.
 
 The algorithm is able to automatically figure out the
 powers of `x`. That is where MEP really shines. We [calculate](app/MainSin.hs)
-30 expressions represented by each chromosome with practically no
+30 expressions represented by each chromosome gene practically with no
 additional computational penalty. Then, we
 choose the best expression. In this run, we have automatically obtained a
 [seventh degree polynomial](https://github.com/masterdezign/hmep/blob/master/doc/sin_approx.py#L12)
@@ -135,6 +135,15 @@ coded by 14 genes. Pretty cool, yeah?
 The result of approximation is [visualized](doc/sin_approx.py) below:
 
 ![Figure](https://github.com/masterdezign/hmep/blob/d173e96acf72e482474e657880f8bd28c40694e7/doc/Figures/sin_approx.png)
+
+From the log below, one can also infer that obtained by MEP
+approximation is better than analytical Taylor
+sine expansions of 3rd and 5th orders, but is worse than the 7th order expansion:
+
+     MEP expression: Average distance for 300 points: 0.0303
+     3rd-order Taylor sine expansion: Average distance for 300 points: 0.3633
+     5rd-order Taylor sine expansion: Average distance for 300 points: 0.0688
+     7rd-order Taylor sine expansion: Average distance for 300 points: 0.0079
 
 
 ## Authors
--- doc/sin_approx.py
+++ doc/sin_approx.py
@@ -25,14 +25,42 @@ def approx(x0):
     v25 = v21 + v20
     return v18 + v25
 
+def taylor3(x):
+    fact3 = 6.0
+    return x - x**3/fact3
+
+def taylor5(x):
+    fact5 = 120.0
+    return taylor3(x) + x**5/fact5
+
+def taylor7(x):
+    fact7 = 5040.0
+    return taylor5(x) - x**7/fact7
+
 def main():
     xs = linspace(-pi,pi,300)
     a = [approx(x) for x in xs]
 
-    distances = [dist1(approx(x), sin(x)) for x in xs]
-    avg = sum(distances) / len(xs)
-    print("Average distance for %d points: %.4f" % (len(xs), avg))
-    # Average distance for 300 points: 0.0303
+    distances1 = [dist1(approx(x), sin(x)) for x in xs]
+    distances2 = [dist1(taylor3(x), sin(x)) for x in xs]
+    distances3 = [dist1(taylor5(x), sin(x)) for x in xs]
+    distances4 = [dist1(taylor7(x), sin(x)) for x in xs]
+
+    avg = sum(distances1) / len(xs)
+    print("MEP expression: Average distance for %d points: %.4f" % (len(xs), avg))
+    # MEP expression: Average distance for 300 points: 0.0303
+
+    avg = sum(distances2) / len(xs)
+    print("3rd-order Taylor sine expansion: Average distance for %d points: %.4f" % (len(xs), avg))
+    # 3rd-order Taylor sine expansion: Average distance for 300 points: 0.3633
+    avg = sum(distances3) / len(xs)
+
+    print("5rd-order Taylor sine expansion: Average distance for %d points: %.4f" % (len(xs), avg))
+    # 5rd-order Taylor sine expansion: Average distance for 300 points: 0.0688
+
+    avg = sum(distances4) / len(xs)
+    print("7rd-order Taylor sine expansion: Average distance for %d points: %.4f" % (len(xs), avg))
+    # 7rd-order Taylor sine expansion: Average distance for 300 points: 0.0079
 
     # Visualization
 
